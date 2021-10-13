3c43a4dcb1cedabd03d9d126ea8c3998e32a1699
Tabriz H. Guliyev <tabrizhg@code.local>
2021-10-13 21:31:37 +0400

    sinifdeki

---
+++ lessontask.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Figure
+    {
+        public int Type { get;  }
+
+        public Figure(int type)
+        {
+            Type = type;
+        }
+        public virtual void CakculateArea()
+        {
+            return;
+        }
+    }
+}
